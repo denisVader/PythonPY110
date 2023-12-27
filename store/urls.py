5d983a0e644deb692d12a1f8921c8f087b1f7aef
denisVader <vadea@mail.ru>
2023-12-27 20:58:06 +0300

    7.

--- store/urls.py
+++ store/urls.py
@@ -7,10 +7,10 @@ app_name = 'store'
 
 urlpatterns = [
     path('product/', products_view),
-    path('', shop_view),
     path('product/<slug:page>.html/', products_page_view, name="products_page_view"),
     path('product/<int:page>/', products_page_view),
-    path('cart/', cart_view),
+    path('cart/', cart_view, name="cart_view"),
     path('cart/add/<str:id_product>/', cart_add_view),
     path('cart/del/<str:id_product>/', cart_del_view),
+    path('', shop_view, name="shop_view")
 ]
--- store/views.py
+++ store/views.py
@@ -34,13 +34,23 @@ def products_view(request):
                                                                  'indent': 4})
 
 
-
 def shop_view(request):
     if request.method == "GET":
-        # with open('store/shop.html', encoding="utf-8") as f:
-        #     data = f.read()  # Читаем HTML файл
-        # return HttpResponse(data)  # Отправляем HTML файл как ответ
-        return render(request, 'store/shop.html', context={"products": DATABASE.values()})
+        # Обработка фильтрации из параметров запроса
+        category_key = request.GET.get("category")
+        if ordering_key := request.GET.get("ordering"):
+            if request.GET.get("reverse") in ('true', 'True'):
+                data = filtering_category(DATABASE, category_key, ordering_key,
+                                          True)
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key)
+        else:
+            data = filtering_category(DATABASE, category_key)
+        return render(request, 'store/shop.html',
+                      context={"products": data, "category": category_key})
+# def shop_view(request):
+#     if request.method == "GET":
+#         return render(request, 'store/shop.html', context={"products": DATABASE.values()})
 
 def products_page_view(request, page):
     if request.method == "GET":
@@ -74,18 +84,19 @@ def cart_view(request):
                                                          'indent': 4})
         products = []  # Список продуктов
         for product_id, quantity in data['products'].items():
-            product = ...  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
+            product = DATABASE.get(product_id)  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
+            product["quantity"] = quantity
             # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
-            product[
-                "price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
+            product["price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
             # 3. добавьте product в список products
-        return render(request, "store/cart.html")
+            products.append(product)
+        return render(request, "store/cart.html", {"products": products})
 
 def cart_add_view(request, id_product):
     if request.method == "GET":
         result = add_to_cart(id_product)    #  Вызвать ответственную за это действие функцию и передать необходимые параметры
         if result:
-            return JsonResponse({"answer": "Продукт успешно добавлен в корзину"},
+            return JsonResponse({"answer": f"Продукт id={id_product} успешно добавлен в корзину"},
                                 json_dumps_params={'ensure_ascii': False})
 
         return JsonResponse({"answer": "Неудачное добавление в корзину"},
