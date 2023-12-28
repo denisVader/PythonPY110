2b4090bd96f11172c67ee2d0b56dfdd1a2b23f4f
DENVALI <vadera@mail.ru>
2023-12-28 18:36:00 +0300

    7 commit

--- store/views.py
+++ store/views.py
@@ -106,7 +106,7 @@ def cart_add_view(request, id_product):
 
 def cart_del_view(request, id_product):
     if request.method == "GET":
-        result = remove_from_cart(id_product) # TODO Вызвать ответственную за это действие функцию и передать необходимые параметры
+        result = remove_from_cart(id_product) #  Вызвать ответственную за это действие функцию и передать необходимые параметры
         if result:
             return JsonResponse({"answer": "Продукт успешно удалён из корзины"},
                                 json_dumps_params={'ensure_ascii': False})
@@ -114,3 +114,25 @@ def cart_del_view(request, id_product):
         return JsonResponse({"answer": "Неудачное удаление из корзины"},
                             status=404,
                             json_dumps_params={'ensure_ascii': False})
+
+def coupon_check_view(request, name_coupon):
+    # DATA_COUPON - база данных купонов: ключ - код купона (name_coupon); значение - словарь со значением скидки в процентах и
+    # значением действителен ли купон или нет
+    DATA_COUPON = {
+        "coupon": {
+            "value": 10,
+            "is_valid": True},
+        "coupon_old": {
+            "value": 20,
+            "is_valid": False},
+    }
+    if request.method == "GET":
+        data = request.Get
+        name_coupon = data.get('coupon')
+        if DATA_COUPON.get(name_coupon):
+            return JsonResponse({"discount": DATA_COUPON["coupon"]["coupon_old"]}, json_dumps_params={'ensure_ascii': False})
+        return HttpResponseNotFound("Неверный купон")
+        # TODO Проверьте, что купон есть в DATA_COUPON, если он есть, то верните JsonResponse в котором по ключу "discount"
+        # получают значение скидки в процентах, а по ключу "is_valid" понимают действителен ли купон или нет (True, False)
+
+        #  Если купона нет в базе, то верните HttpResponseNotFound("Неверный купон")
